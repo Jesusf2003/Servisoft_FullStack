a384a9bb9b5dfca4d551638c796981cd472d60ca
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2021-12-21 18:12:30 -0500

    Servisoft: Proyecto inicial de Spring Boot.

---
+++ servisoft/src/test/java/com/springboot/servisoft/ServisoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.servisoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServisoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
