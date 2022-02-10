511e0e002a7987edc3a7c4385b9c3ce6edfc24f7
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-02-10 10:16:01 -0500

    Vaciando la rama

--- servisoft/src/main/java/com/springboot/servisoft/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.servisoft.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.springboot.servisoft.model.Persona;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long>{
-}
--- servisoft/src/main/java/com/springboot/servisoft/repository/UbigeoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.servisoft.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.springboot.servisoft.model.Ubigeo;
-
-public interface UbigeoRepository extends JpaRepository<Ubigeo, String>{
-}
